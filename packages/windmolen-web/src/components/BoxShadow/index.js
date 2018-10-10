fa440922845cd650545067efba556375d252cd33
Bruno Queiros <brunoofq@gmail.com>
2018-10-10 12:39:15 +0200

    BoxShadow small shadow variant

--- packages/windmolen-web/src/components/BoxShadow/index.js
+++ packages/windmolen-web/src/components/BoxShadow/index.js
@@ -5,26 +5,34 @@ import styled from 'styled-components';
 
 export type Props = {
   children?: Node,
-  wrapper: Node
+  wrapper: Node,
+  small?: Boolean
 };
 
 const StyledBoxShadow = ({ wrapper, ...props }: Props) => {
   const Element = styled(wrapper).attrs(props)`
-    box-shadow: -20px 14px 20px 0 rgba(0, 0, 0, 0.18);
     display: inline-block;
     background-color: #fff;
 
-    ${media.desktop`
+    ${props => !props.small && `
+      box-shadow: -20px 14px 20px 0 rgba(0, 0, 0, 0.18);
+    `}
+
+    ${props => !props.small && media.desktop`
       box-shadow: -40px 28px 40px 0 rgba(0, 0, 0, 0.18);
+    `}
+
+    ${props => props.small && `
+      box-shadow: 0 0 5px 0 rgba(51, 61, 71, 0.12);
     `}
   `;
 
   return <Element />;
 };
 
-const BoxShadow = ({ children }: Props) => {
+const BoxShadow = ({ children, small }: Props) => {
   return React.Children.map(children, (child) => (
-    <StyledBoxShadow {...child.props} wrapper={child.type} />
+    <StyledBoxShadow {...child.props} small={small} wrapper={child.type} />
   ));
 };
 
