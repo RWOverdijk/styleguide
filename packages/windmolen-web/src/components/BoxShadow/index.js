14321758df1e9eb38926640a7fd37802b33a6923
Bruno Queiros <brunoofq@gmail.com>
2018-10-12 11:14:08 +0200

    Render a div instead of apply style to the child element

--- packages/windmolen-web/src/components/BoxShadow/index.js
+++ packages/windmolen-web/src/components/BoxShadow/index.js
@@ -5,35 +5,33 @@ import styled from 'styled-components';
 
 export type Props = {
   children?: Node,
-  wrapper: Node,
   small?: Boolean
 };
 
-const StyledBoxShadow = ({ wrapper, ...props }: Props) => {
-  const Element = styled(wrapper).attrs(props)`
-    display: inline-block;
-    background-color: #fff;
+const StyledBoxShadow = styled.div`
+  display: inline-block;
+  background-color: #fff;
 
-    ${props => !props.small && `
-      box-shadow: -20px 14px 20px 0 rgba(0, 0, 0, 0.18);
-    `}
+  ${props => !props.small && `
+    box-shadow: -20px 14px 20px 0 rgba(0, 0, 0, 0.18);
+  `}
 
-    ${props => !props.small && media.desktop`
-      box-shadow: -40px 28px 40px 0 rgba(0, 0, 0, 0.18);
-    `}
+  ${props => !props.small && media.desktop`
+    box-shadow: -40px 28px 40px 0 rgba(0, 0, 0, 0.18);
+  `}
 
-    ${props => props.small && `
-      box-shadow: 0 0 5px 0 rgba(51, 61, 71, 0.12);
-    `}
-  `;
+  ${props => props.small && `
+    box-shadow: 0 0 5px 0 rgba(51, 61, 71, 0.12);
+  `}
 
-  return <Element />;
-};
+  img,
+  iframe {
+    vertical-align: middle;
+  }
+`;
 
-const BoxShadow = ({ children, small }: Props) => {
-  return React.Children.map(children, (child) => (
-    <StyledBoxShadow {...child.props} small={small} wrapper={child.type} />
-  ));
-};
+const BoxShadow = ({ children, small }: Props) => (
+  <StyledBoxShadow small={small}>{children}</StyledBoxShadow>
+);
 
 export default BoxShadow;
