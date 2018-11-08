179b45c6f7116e71e22f66b7dcfa4cd3cf497a82
alexsyo <alex_ok@hotmail.it>
2018-11-08 15:28:33 +0100

    error component exported correctly
    feature: set-up-v2

--- packages/windmolen-web/src/index.js
+++ packages/windmolen-web/src/index.js
@@ -34,4 +34,4 @@ export { default as Toggle } from './components/Form/Toggle';
 export { default as Radio } from './components/Form/Radio';
 export { default as RadioGroup } from './components/Form/RadioGroup';
 export { default as Select } from './components/Form/Select';
-export { default as Error } from '.components/Form/Error';
+export { default as Error } from './components/Form/Error';
